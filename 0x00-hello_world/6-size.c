06c9d895762cf8f9787787d745b06e9284ab2e9f
Miromah <marmarika56@gmail.com>
2023-07-13 19:31:46 -0700

    Task completed

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with the put function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of char: %u byte(s)\n", sizeof(char));
+printf("Size of int: %u byte(s)\n", sizeof(int));
+printf("Size of long int : %u byte(s)\n", sizeof(long int));
+printf("Size of long long int : %u byte(s)\n", sizeof(long long int));
+printf("Size of float : %u byte(s)\n", sizeof(float));
+return (0);
+}
